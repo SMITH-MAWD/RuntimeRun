0c2b62130e4949da86c20b59315bcb21fc71fe33
SMITH-MAWD <Sonjames1127@gmail.com>
2026-02-02 22:04:26 +0800

    elevator

--- Project Runtime/Assets/elevator.cs
+++ Project Runtime/Assets/elevator.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class elevator : MonoBehaviour
 {
+    public Transform pointA;
+    public Transform pointB;
+    public float speed = 20f;
+    private Vector3 nextPosition;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        nextPosition = pointB.position;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, nextPosition, speed * Time.deltaTime);
+        if (transform.position == nextPosition)
+        {
+            nextPosition = (nextPosition == pointA.position) ? pointB.position : pointA.position;
+        }
     }
 }
