c6773d59f3be810e02110a1c1cd30a2cd430c859
JJ-ed <jirehniquolae.t.vailoces@gmail.com>
2026-05-14 22:24:52 +0800

    Slight Elevator fix

--- Project Runtime/Assets/elevator.cs
+++ Project Runtime/Assets/elevator.cs
@@ -1,25 +1,59 @@
 using UnityEngine;
 
+
+[RequireComponent(typeof(Rigidbody2D))]
 public class elevator : MonoBehaviour
 {
     public Transform pointA;
     public Transform pointB;
     public float speed = 20f;
     private Vector3 nextPosition;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Rigidbody2D _rb;
+
+    private bool _playerOnboard;
+    private const float ArrivalThreshold = 0.01f;
+
+
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _rb.bodyType = RigidbodyType2D.Kinematic;
+    }
+
     void Start()
     {
         nextPosition = pointB.position;
-
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.position = Vector3.MoveTowards(transform.position, nextPosition, speed * Time.deltaTime);
-        if (transform.position == nextPosition)
+        if (!_playerOnboard)
+            return;
+
+        Vector3 target = Vector3.MoveTowards(_rb.position, nextPosition, speed * Time.fixedDeltaTime);
+        _rb.MovePosition(target);
+
+        if (Vector2.Distance(_rb.position, nextPosition) <= ArrivalThreshold)
         {
             nextPosition = (nextPosition == pointA.position) ? pointB.position : pointA.position;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.parent = transform;
+            _playerOnboard = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.parent = null;
+            _playerOnboard = false;
         }
     }
 }
